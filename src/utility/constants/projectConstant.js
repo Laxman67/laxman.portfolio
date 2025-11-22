f1f9daa8cfd918f9e3153403780ec8ee62e48fff
Laxman <laxmanhenricks94@gmail.com>
2025-11-23 01:04:20 +0530

    added 3M - My Meaning Finder Project

--- src/utility/constants/projectConstant.js
+++ src/utility/constants/projectConstant.js
@@ -1,49 +1,62 @@
 const projects = [
   {
-    title: "AI Voice Recruiter",
+    title: 'AI Voice Recruiter',
     description:
-      "An AI-powered voice recruitment assistant that automates candidate screening, evaluates responses in real-time, and generates structured interview reports. Built with modern full-stack technologies and seamless speech-to-text integration.",
-    tech: ["Nextjs", "Node.js", "Supabse", "Google Cloud Firebase", "ShadCn"],
-    github: "https://github.com/Laxman67/AI-Voice-Agent",
-    live: "#",
-    imageLink: "./ai-agent-interviewer.png",
+      'An AI-powered voice recruitment assistant that automates candidate screening, evaluates responses in real-time, and generates structured interview reports. Built with modern full-stack technologies and seamless speech-to-text integration.',
+    tech: ['Nextjs', 'Node.js', 'Supabse', 'Google Cloud Firebase', 'ShadCn'],
+    github: 'https://github.com/Laxman67/AI-Voice-Agent',
+    live: '#',
+    imageLink: './ai-agent-interviewer.png',
   },
   {
-    title: "QAAudit System",
+    title: 'QAAudit System',
     description:
-      "An intelligent QA auditing platform that streamlines application assessments by generating automated reports, analyzing sections with weighted scoring, and providing clear summaries, best practices, and improvement recommendations. Fully integrated with GPT-powered insights and a simplified audit schema.",
-    tech: ["React", "Express", "Nodejs", "MongoDB"],
-    github: "https://github.com/Laxman67/QASSERT_QAAudit",
-    live: "https://qassert-qa-audit.vercel.app/",
-    imageLink: "./qa-audit-system.png",
+      'An intelligent QA auditing platform that streamlines application assessments by generating automated reports, analyzing sections with weighted scoring, and providing clear summaries, best practices, and improvement recommendations. Fully integrated with GPT-powered insights and a simplified audit schema.',
+    tech: ['React', 'Express', 'Nodejs', 'MongoDB'],
+    github: 'https://github.com/Laxman67/QASSERT_QAAudit',
+    live: 'https://qassert-qa-audit.vercel.app/',
+    imageLink: './qa-audit-system.png',
   },
   {
-    title: "JIRA Plugin – Test Case & User Story Generator",
+    title: 'JIRA Plugin – Test Case & User Story Generator',
     description:
-      "Forge-based Jira automation plugin that generates test cases and user stories from epics, links them for complete traceability, and aligns with Atlassian Marketplace standards. Includes integration testing, UI refinement, and a dedicated support portal for issue reporting.",
-    tech: ["Forge", "Atlassian API", "React", "JavaScript", "REST API"],
+      'Forge-based Jira automation plugin that generates test cases and user stories from epics, links them for complete traceability, and aligns with Atlassian Marketplace standards. Includes integration testing, UI refinement, and a dedicated support portal for issue reporting.',
+    tech: ['Forge', 'Atlassian API', 'React', 'JavaScript', 'REST API'],
     github:
-      "https://github.com/Laxman67/QAssert-TestCase-and-UserStory-Generator",
-    live: "https://marketplace.atlassian.com/apps/1237432/qassert-testcase-and-userstory-generator?hosting=cloud&tab=support",
-    imageLink: "./testcase-useStory.png",
+      'https://github.com/Laxman67/QAssert-TestCase-and-UserStory-Generator',
+    live: 'https://marketplace.atlassian.com/apps/1237432/qassert-testcase-and-userstory-generator?hosting=cloud&tab=support',
+    imageLink: './testcase-useStory.png',
   },
   {
-    title: "Dynamic Product List",
+    title: 'Dynamic Product List',
     description:
-      "Interactive product listing app with category-wise filtering and dynamic rendering using placeholder JSON data from a free Fake REST API. Built with React and Tailwind CSS, featuring a clean and responsive UI.",
-    tech: ["React", "Tailwind CSS"],
-    github: "https://github.com/Laxman67/ProductList",
-    live: "https://productlist-i49b.onrender.com/",
-    imageLink: "./dynamic-product-list.png",
+      'Interactive product listing app with category-wise filtering and dynamic rendering using placeholder JSON data from a free Fake REST API. Built with React and Tailwind CSS, featuring a clean and responsive UI.',
+    tech: ['React', 'Tailwind CSS'],
+    github: 'https://github.com/Laxman67/ProductList',
+    live: 'https://productlist-i49b.onrender.com/',
+    imageLink: './dynamic-product-list.png',
   },
   {
-    title: "ClothHUB – Fashion Product Listing",
+    title: 'ClothHUB – Fashion Product Listing',
     description:
-      "A modern fashion product listing interface featuring clean design, responsive layouts, and fast-rendering components built with React and Tailwind CSS.",
-    tech: ["React", "Tailwind CSS"],
-    github: "https://github.com/Laxman67/crwn-clothing-v2",
-    live: "https://rainbow-speculoos-0488ea.netlify.app/",
-    imageLink: "./cloth-hub.png",
+      'A modern fashion product listing interface featuring clean design, responsive layouts, and fast-rendering components built with React and Tailwind CSS.',
+    tech: ['React', 'Tailwind CSS'],
+    github: 'https://github.com/Laxman67/crwn-clothing-v2',
+    live: 'https://rainbow-speculoos-0488ea.netlify.app/',
+    imageLink: './cloth-hub.png',
+  },
+  {
+    title: '3M - My Meaning Finder',
+    description:
+      ' A lightweight, in-page dictionary tool that instantly displays the definition of a selected word or short phrase. It activates on the mouseup event, fetching the definition from an external dictionary API. If a definition is found, a temporary popup is displayed near the cursor, and the definition is read aloud using the Web Speech API. The popup is automatically dismissed when the user clicks elsewhere. ',
+    tech: [
+      'Vanilla JavaScript',
+      'Web Speech API',
+      'External Dictionary API (dictionaryapi.dev)',
+    ],
+    github: 'https://github.com/Laxman67/My-Meaning-Finder',
+    live: 'https://github.com/Laxman67/My-Meaning-Finder',
+    imageLink: './meaning-finder-screenshot.png',
   },
 ];
 
